0cbd41e31c310063c63d0d332ccfefa613f570f4
xyasth <pkosasih@student.ciputra.ac.id>
2025-11-13 12:03:37 +0700

    feature: added schedule page and scheduleviewcontroller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ScheduleViewController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,4 +27,5 @@ Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/schedule', [ScheduleViewController::class, 'showSchedulePage'])->name('schedule.page');
 });
