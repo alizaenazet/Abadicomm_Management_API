3e69e7ba832774bf39746e51dbc1c31e10cc6a3e
alizaenazet <alizaenazet@icloud.com>
2025-12-16 23:54:20 +0700

    feat: Implement password reset functionality with email notifications

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,12 @@ Route::get('/', function () {
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
+// Password Reset Routes
+Route::get('/forgot-password', [AuthController::class, 'showForgotPassword'])->name('password.request');
+Route::post('/forgot-password', [AuthController::class, 'sendResetLink'])->name('password.email');
+Route::get('/reset-password/{token}', [AuthController::class, 'showResetPassword'])->name('password.reset');
+Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.update');
+
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // Protected Routes
